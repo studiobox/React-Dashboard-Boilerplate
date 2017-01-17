a6f78a257372703d2e6ca4512534245539d6ffaf
studiobox <kalpak.palekar@gmail.com>
2017-01-17 14:14:43 +0530

    First Commit

---
+++ dev/js/index.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div>
+        <h1>Welcome to React Dashboard Boilerplate</h1>
+        <p>A basic project to use for any type of dashboard project. This basic boilerplate is wraped with React and Redux and basic HTML5/CSS3 configuration.</p>
+    </div>,
+    document.getElementById('app')
+)
