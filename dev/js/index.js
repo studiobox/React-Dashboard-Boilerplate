22ea17f261f704e3c5b677918ad2f9f50fa7307b
studiobox <kalpak.palekar@gmail.com>
2017-01-17 18:25:59 +0530

    one more

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,10 +1,15 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styles from '../css/main.css'
+import componentStyles from '../css/component.css'
+import img from '../images/final-logo.png'
+import consulting from '../images/consulting_service.jpg'
 
 ReactDOM.render(
     <div>
-        <h1>Welcome to React Dashboard Boilerplate</h1>
+        <h1 className="welcome-head">Welcome to React Dashboard Boilerplate</h1>
+        <img src={consulting}/>
         <p>A basic project to use for any type of dashboard project. This basic boilerplate is wraped with React and Redux and basic HTML5/CSS3 configuration.</p>
     </div>,
     document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devServer: {
@@ -15,6 +16,14 @@ module.exports = {
                 test: /\.js$/,
                 loaders: ['babel'],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("css")
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "file-loader?name=/images/[name].[ext]"
             }
         ]
     },
@@ -23,6 +32,7 @@ module.exports = {
         filename: 'js/bundle.min.js'
     },
     plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new ExtractTextPlugin("css/style.css")
     ]
 };
